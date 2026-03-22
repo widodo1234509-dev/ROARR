cb2a4ec6d16917e98211ef262b561ed9848e97eb
widodo1234509-dev <widodo1234509@gmail.com>
2026-03-22 18:36:24 +0700

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CVAI4/BloxFruits/main/SeaDominatorV4.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/widdo01234509-dev/ROARR/main/loader.lua"))()
